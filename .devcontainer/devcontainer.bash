1752e9398b85acd680179a15de1144a922ca4469
Daria Shevchenko <daria.pinkfloyd@yandex.ru>
2024-12-09 21:42:28 +0300

    delete comments

--- .devcontainer/devcontainer.bash
+++ .devcontainer/devcontainer.bash
@@ -2,13 +2,10 @@
 
 set -e
 
-# Перейти в директорию скрипта
 cd "$(dirname "$0")"
 
-# Обновление пакетов
 apt update
 
-# Установка основных инструментов разработки
 apt install -y \
     cmake \
     git \
@@ -23,13 +20,10 @@ apt install -y \
     linux-tools-common \
     linux-tools-generic
 
-# Установка perf (обновите путь, если версия отличается)
 cp -f /usr/lib/linux-tools/6.11.0-9-generic/perf /usr/bin/perf
 
-# Установка Google Test
 apt install -y libgtest-dev
 
-# Сборка Google Test (он поставляется только в виде исходников)
 cd /usr/src/gtest
 cmake . 
 make
